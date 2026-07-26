7c278764486eec4bd9981820ffc2b84ddb762e38
Jonny Candes <arjscandes73@gmail.com>
2026-07-26 17:52:58 +0800

    delete

--- tests/setup.ts
+++
@@ -1,7 +0,0 @@
-import { beforeAll } from "vitest";
-
-beforeAll(() => {
-  process.env.GITHUB_APP_ID = "123";
-  process.env.GITHUB_PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----\ndummy\n-----END RSA PRIVATE KEY-----";
-  process.env.GITHUB_WEBHOOK_SECRET = "secret";
-});
